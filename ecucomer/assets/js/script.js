b60ff1870a1f7a5fb0776a094acea8b50053c975
JsGv1405 <86036587+JsGv1405@users.noreply.github.com>
2022-11-06 21:21:26 -0500

    Add files via upload

---
+++ ecucomer/assets/js/script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+
+
+/**
+ * #PRELOADING
+ */
+
+const loadElement = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", function () {
+  loadElement.classList.add("loaded");
+});
+
+
+
+/**
+ * #MOBILE NAVBAR TOGGLE
+ */
+
+
+
+
+
+/**
+ *  #HEADER
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-go-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * #SCROLL REVEAL
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const scrollReveal = function () {
+  for (let i = 0, x = revealElements.length; i < x; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      revealElements[i].classList.add("revealed");
+    } else {
+      revealElements[i].classList.remove("revealed");
+    }
+  }
+}
+
+window.addEventListener("scroll", scrollReveal);
+window.addEventListener("load", scrollReveal);
